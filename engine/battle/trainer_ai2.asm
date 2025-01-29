0b13fdb2409f9c590c6612f734314de56063d104
Narishma-gb <sw.hunandin@gmail.com>
2025-01-29 14:35:42 +0100

    Add .gitignore, .gitattributes files

--- engine/battle/trainer_ai2.asm
+++ engine/battle/trainer_ai2.asm
@@ -350,7 +350,7 @@ AICureStatus:
 	res BADLY_POISONED, [hl]
 	ret
 
-AIUseXAccuracy: ; unused
+AIUseXAccuracy: ; unreferenced
 	call AIPlayRestoringSFX
 	ld hl, wEnemyBattleStatus2
 	set USING_X_ACCURACY, [hl]
@@ -364,7 +364,7 @@ AIUseGuardSpec:
 	ld a, GUARD_SPEC
 	jp AIPrintItemUse
 
-AIUseDireHit: ; unused
+AIUseDireHit: ; unreferenced
 	call AIPlayRestoringSFX
 	ld hl, wEnemyBattleStatus2
 	set GETTING_PUMPED, [hl]
